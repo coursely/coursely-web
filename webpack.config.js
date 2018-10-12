b9b8910f65e3ee4c2416724aa172a4a3054a9c55
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-12 07:10:37 -0700

    TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CheckerPlugin } = require("awesome-typescript-loader");
 
-module.exports = { plugins: [new HtmlWebpackPlugin({ title: "Coursely" })] };
+module.exports = {
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  devtool: "source-map",
+  module: { rules: [{ test: /\.tsx?/, loader: "awesome-typescript-loader" }] },
+  plugins: [new CheckerPlugin(), new HtmlWebpackPlugin({ title: "Coursely" })]
+};
