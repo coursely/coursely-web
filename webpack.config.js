9cee5d34459c90523e4ca84c15a6ba66403b0239
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-12 07:21:46 -0700

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CheckerPlugin } = require("awesome-typescript-loader");
 
 module.exports = {
   resolve: { extensions: [".ts", ".tsx", ".js"] },
   devtool: "source-map",
+  devServer: { contentBase: "./dist" },
   module: { rules: [{ test: /\.tsx?/, loader: "awesome-typescript-loader" }] },
-  plugins: [new CheckerPlugin(), new HtmlWebpackPlugin({ title: "Coursely" })]
+  plugins: [
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({ title: "Coursely" }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
