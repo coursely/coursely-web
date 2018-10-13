2fea9adbfa80e2c43433f6905217018032b5ff98
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-12 22:50:51 -0700

    Split dev and prod Webpack configs

---
+++ webpack.dev.js
@@ -0,0 +1,9 @@
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const config = require("./webpack.common.js");
+
+module.exports = merge(config, {
+  mode: "development",
+  devServer: { contentBase: "./dist" },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+});
