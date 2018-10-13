83f30a2cfa49162bd7d4cd5aa21c9fd3cd3449ce
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-12 22:50:51 -0700

    Split dev and prod Webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CheckerPlugin } = require("awesome-typescript-loader");
@@ -6,12 +5,10 @@ const { CheckerPlugin } = require("awesome-typescript-loader");
 module.exports = {
   resolve: { extensions: [".ts", ".tsx", ".js"] },
   devtool: "source-map",
-  devServer: { contentBase: "./dist" },
   module: { rules: [{ test: /\.tsx?/, loader: "awesome-typescript-loader" }] },
   plugins: [
     new CheckerPlugin(),
     new CleanWebpackPlugin(["dist"]),
-    new HtmlWebpackPlugin({ title: "Coursely" }),
-    new webpack.HotModuleReplacementPlugin()
+    new HtmlWebpackPlugin({ title: "Coursely" })
   ]
 };
