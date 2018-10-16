7508e9017628b807c37ce1a412b01f5d2a7a9bd9
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-15 20:31:58 -0700

    Inferno
    ATL's CheckerPlugin isn't compatible with ts-transform-inferno, so
    remove it.

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-function component() {
-  let element = document.createElement("div");
-  element.innerHTML = "Hello, Webpack!";
-  return element;
-}
-
-console.log("Hello, Webpack!");
-document.body.appendChild(component());
--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,26 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
+const transformInferno = require("ts-transform-inferno").default;
 
 module.exports = {
-  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] },
   devtool: "source-map",
-  module: { rules: [{ test: /\.tsx?/, loader: "awesome-typescript-loader" }] },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        options: {
+          getCustomTransformers: program => ({
+            before: [transformInferno()]
+          }),
+          reportFiles: ["src/**/*.{ts,tsx}"]
+        }
+      }
+    ]
+  },
   plugins: [
-    new CheckerPlugin(),
     new CleanWebpackPlugin(["dist"]),
-    new HtmlWebpackPlugin({ title: "Coursely" })
+    new HtmlWebpackPlugin({ title: "Coursely", template: "public/index.html" })
   ]
 };
