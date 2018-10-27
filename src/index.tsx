1de62c6367d484abc1360663a37a86488490d313
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-27 15:34:36 -0700

    Ava

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-class App extends React.Component {
+export class App extends React.Component {
   render() {
     return <div>Hello, React!</div>;
   }
---
+++ test/index.test.tsx
@@ -0,0 +1,3 @@
+import test from "ava";
+
+test("renders the app", p => p.pass());
