46579f909516a8e6565de017dd881942602b8dfc
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-27 11:41:22 -0700

    Replace Inferno with React
    Fixes #10.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const transformInferno = require("ts-transform-inferno").default;
 
 module.exports = {
   resolve: { extensions: [".ts", ".tsx", ".js", ".jsx"] },
@@ -10,17 +9,11 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: "awesome-typescript-loader",
-        options: {
-          getCustomTransformers: program => ({
-            before: [transformInferno()]
-          }),
-          reportFiles: ["src/**/*.{ts,tsx}"]
-        }
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(["dist"]),
-    new HtmlWebpackPlugin({ title: "Coursely", template: "public/index.html" })
-  ]
+    new HtmlWebpackPlugin({ title: "Coursely", template: "public/index.html" }),
+  ],
 };
