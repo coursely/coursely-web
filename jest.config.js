a39d22cf4072a2e7214c043714b50ba8ff3b2ee9
Jesse B. Hannah <jesse@jbhannah.net>
2018-10-29 12:19:39 -0700

    Move Enzyme configuration to setup script

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
   coveragePathIgnorePatterns: ["<rootDir>/src/index.tsx"],
   preset: "ts-jest",
+  setupTestFrameworkScriptFile: "<rootDir>/jest.setup.ts",
   testEnvironment: "node",
 };
--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,6 @@
-import { configure, shallow } from "enzyme";
-import * as Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import * as React from "react";
 import { App } from "./App";
-
-configure({ adapter: new Adapter() });
 
 it("renders the app", () => {
   const component = shallow(<App />);
